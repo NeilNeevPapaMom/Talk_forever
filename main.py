9dc14f1a6bb94261d1e97c1a405b7d5d7f1595ea
NeilNeev <102471194+NeilNeevPapaMom@users.noreply.github.com>
2022-10-20 16:21:06 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask_socketio import SocketIO, send
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET'] = 'secret!123'
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+
+@socketio.on('message')
+def handle_message(message):
+    print('Received message: ' + message)
+    if message != 'User connected!':
+        send(message, broadcast=True)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    socketio.run(app, host='192.168.0.168',debug=True, allow_unsafe_werkzeug=True)
